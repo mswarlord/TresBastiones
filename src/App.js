8732f9604a4d3eb70f28fa75f771cfdbbae11075
Facundo Cantero Rodriguez <49701336+mswarlord@users.noreply.github.com>
2022-08-24 20:45:28 -0300

    Creación ItemListContainer
    Se incorporó el componente ItemListContainer para la entrega del TP Nro
    3.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 import BootstrapNavBar from './Componentes/BoostrapNavBar';
-
+import ItemListContainer from './Componentes/ItemListContainer';
 
 function App() {
   return (
     <div>
       <BootstrapNavBar /> 
+      <listContainer className="ItemList">
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+        <ItemListContainer />
+      </listContainer>
     </div>
   );
 }
---
+++ src/Componentes/ItemListContainer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ItemListContainer.css';
+
+export const ItemListContainer = () => {
+    return(
+        <div className='ItemListContainer'>
+            Item List Container
+        </div> 
+    )
+}
+
+export default ItemListContainer;
